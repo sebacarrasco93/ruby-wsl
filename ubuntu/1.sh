754e612ce220c1942bcbdf0906068f7e890d6cd6
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2022-08-09 14:40:04 -0400

    Separar archivos 1 y 2

--- ubuntu/1.sh
+++ ubuntu/1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Intentando instalar clave desde servidor 1..."
 #gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -11,3 +11,8 @@ echo "Intentando instalar clave desde servidor 4..."
 
 echo "Intentando instalar gestor de paquetes RVM + Ruby on Rails..."
 \curl -sSL https://get.rvm.io | bash -s stable --rails
+
+echo "Agregando el source de RVM"
+/home/mari/.rvm/scripts/rvm
+
+echo "Ahora debes cerrar esta ventana, y volver a abrirla..."
---
+++ ubuntu/2.sh
@@ -0,0 +1,8 @@
+echo "Instalando Ruby on Rails"
+gem install rails
+
+echo "Listo! Ahora debería funcionar"
+echo "Prueba crear una carpeta y escribir:"
+echo
+echo "rails new tuproyecto -T"
+echo "(recuerda cambiar el nombre de tuproyecto por uno propio"
